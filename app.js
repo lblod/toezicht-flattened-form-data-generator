45146163c78c6bbc73bc0c2127cf3bbc39e52807
Dries Hugaerts <dries.hugaerts@qmino.com>
2020-02-24 10:37:57 +0100

    Added TODO list, to get a better understanding of what needs to be done.

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
-import { app } from 'mu';
+import {app} from 'mu';
 
-app.get('/', function( req, res ) {
+app.get('/', function (req, res) {
     res.send('Hello toezicht-flattened-form-data-generator');
-} );
+});
+
+app.post('/delta', async function (req, res, next) {
+
+    // TODO find the submission doc/file URI related to the submission following the path `?submission dct:subject ?submittedDocument`
+
+    // TODO retrieve the ttl file of the submitted submission. Make sure it is of type `<http://data.lblod.gift/concepts/form-data-file-type>`
+
+    // TODO parse this ttl file using the RDF lib.
+
+    // TODO map the given submission to the triplets we want to save. !! BE AWARE !! Could contain multiple triples per property
+
+    // TODO Insert a new resource of type melding:FormData with a uuid and all properties of the mapping above in the same graph as the submission
+
+    // TODO Relate the form data resource to the submission using ?submission prov:generated ?formData
+
+    // TODO Relate the form data resource to the form data TTL file using ?formData prov:hadPrimarySource ?ttlFile
+});
