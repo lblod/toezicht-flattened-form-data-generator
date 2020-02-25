bdf85566b96473302146a2de0901058c199b47ee
Dries Hugaerts <dries.hugaerts@qmino.com>
2020-02-25 09:36:02 +0100

    temp commit to save current progress.

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 import {app} from 'mu';
 import {TurtleFile} from "./lib/turtle-file";
+import {FormData} from "./lib/form-data";
 
+const OLD_URI = "http://data.lblod.info/forms/meldingsplicht/0711f911-4c75-4097-8cad-616fef08ffcd";
 const TEST_FORM_PATH = "/app/util/form-example.ttl";
 
 app.get('/', function (req, res) {
@@ -10,16 +12,19 @@ app.get('/', function (req, res) {
 app.post('/delta', async function (req, res, next) {
 
     // TODO find the submission doc/file URI related to the submission following the path `?submission dct:subject ?submittedDocument`
-
     // TODO retrieve the ttl file of the submitted submission. Make sure it is of type `<http://data.lblod.gift/concepts/form-data-file-type>`
 
 
-    // const formTTL = await getFile(TEST_FORM_PATH);
-    const form = await new TurtleFile().read(TEST_FORM_PATH);
-    const triples = form.triples;
-    debugger;
+    // submitted ttl
+    const submission = await new TurtleFile().read(TEST_FORM_PATH);
 
-    // TODO parse this ttl file using the RDF lib.
+    // the new form(-data)
+    const form = new FormData(OLD_URI, submission);
+
+    form.process();
+
+    // save the new form
+    form.insert();
 
     // TODO map the given submission to the triplets we want to save. !! BE AWARE !! Could contain multiple triples per property
 
