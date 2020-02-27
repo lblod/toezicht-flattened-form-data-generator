fcd6f7917f33f24d9a3b2dda54dd6408d2a548f4
Dries Hugaerts <dries.hugaerts@qmino.com>
2020-02-27 13:18:04 +0100

    added extractor for session-started-at-time.

--- util/namespaces.js
+++ util/namespaces.js
@@ -5,6 +5,7 @@ export const RDF = Namespace("http://www.w3.org/1999/02/22-rdf-syntax-ns#");
 export const DCT = Namespace("http://purl.org/dc/terms/");
 export const ELOD = Namespace("http://linkedeconomy.org/ontology#");
 export const LBLOD_BESLUIT = Namespace("http://lblod.data.gift/vocabularies/besluit/");
+export const BESLUIT = Namespace("http://data.vlaanderen.be/ns/besluit#");
 export const RDFS = Namespace("http://www.w3.org/2000/01/rdf-schema#");
 export const SCHEMA = Namespace("http://schema.org/");
 export const EXT = Namespace("http://def.seegrid.csiro.au/isotc211/iso19115/2003/extent#");
