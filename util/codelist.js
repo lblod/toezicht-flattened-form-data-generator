48b6b8acec98301c895efcdc5d2d0185b409d5a5
Claire Lovisa <lovisa.claire@gmail.com>
2021-07-28 18:04:14 +0200

    Add extractors for vaststelling gemeenteweg

--- util/codelist.js
+++ util/codelist.js
@@ -4,7 +4,8 @@ import {retrieveCodeListQuery} from "./queries";
 export const DECISION_TYPES = 'http://lblod.data.gift/concept-schemes/71e6455e-1204-46a6-abf4-87319f58eaa5';
 export const REGULATION_TYPES = 'http://lblod.data.gift/concept-schemes/c93ccd41-aee7-488f-86d3-038de890d05a';
 export const TAX_TYPES = 'http://lblod.data.gift/concept-schemes/3037c4f4-1c63-43ac-bfc4-b41d098b15a6';
-
+export const ADOPTION_TYPES = 'http://lblod.data.gift/concept-schemes/a995bb71-3c87-4385-a06b-a786f2fa0d16';
+export const MUNICIPAL_ROAD_PROCEDURE_TYPES = 'http://lblod.data.gift/concept-schemes/60d620a5-ec34-4a91-ba84-fff0813d0ccc';
 
 export async function retrieveCodeList(uri){
   try {
