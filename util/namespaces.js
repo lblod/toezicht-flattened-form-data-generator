a022db6b8bc079503107cfa8782d775b2571b291
benjay10 <ben.behaeghe@redpencil.io>
2022-10-08 05:07:12 +0200

    Linting fixes (manual and automatic)

--- util/namespaces.js
+++ util/namespaces.js
@@ -4,13 +4,17 @@ export const ELI = Namespace('http://data.europa.eu/eli/ontology#');
 export const RDF = Namespace('http://www.w3.org/1999/02/22-rdf-syntax-ns#');
 export const DCT = Namespace('http://purl.org/dc/terms/');
 export const ELOD = Namespace('http://linkedeconomy.org/ontology#');
-export const LBLOD_BESLUIT = Namespace('http://lblod.data.gift/vocabularies/besluit/');
+export const LBLOD_BESLUIT = Namespace(
+  'http://lblod.data.gift/vocabularies/besluit/'
+);
 export const BESLUIT = Namespace('http://data.vlaanderen.be/ns/besluit#');
 export const RDFS = Namespace('http://www.w3.org/2000/01/rdf-schema#');
 export const SCHEMA = Namespace('http://schema.org/');
 export const EXT = Namespace('http://mu.semte.ch/vocabularies/ext/');
-export const MELDING = Namespace('http://lblod.data.gift/vocabularies/automatische-melding/');
+export const MELDING = Namespace(
+  'http://lblod.data.gift/vocabularies/automatische-melding/'
+);
 export const PROV = Namespace('http://www.w3.org/ns/prov#');
 export const ADMS = Namespace('http://www.w3.org/ns/adms#');
 export const XSD = Namespace('http://www.w3.org/2001/XMLSchema#');
-export const TASK = Namespace('http://redpencil.data.gift/vocabularies/tasks/')
+export const TASK = Namespace('http://redpencil.data.gift/vocabularies/tasks/');
--- util/queries.js
+++ util/queries.js
@@ -1,155 +1,166 @@
-import { sparqlEscapeUri, sparqlEscapeString, sparqlEscapeBoolean } from 'mu';
 import { MELDING, PROV } from './namespaces';
+import { sparqlEscapeUri, sparqlEscapeString } from 'mu';
 
-const STATUS_SEND_CONCEPT = "http://lblod.data.gift/concepts/9bd8d86d-bb10-4456-a84e-91e9507c374c";
+const STATUS_SEND_CONCEPT =
+  'http://lblod.data.gift/concepts/9bd8d86d-bb10-4456-a84e-91e9507c374c';
 
 // TODO can this usage of queries be improved?
 
 export function createSubmissionForQuery(uri) {
   return `
-PREFIX dct: <http://purl.org/dc/terms/>
-PREFIX nie: <http://www.semanticdesktop.org/ontologies/2007/01/19/nie#>
+    PREFIX dct: <http://purl.org/dc/terms/>
+    PREFIX nie: <http://www.semanticdesktop.org/ontologies/2007/01/19/nie#>
 
-SELECT ?submission ?logicalFile ?physicalFile ?submittedDocument
-WHERE {
-  BIND (${sparqlEscapeUri(uri)} as ?submission)
-  ?submission dct:subject ?submittedDocument .
-  ?submittedDocument dct:source ?logicalFile .
-  ?logicalFile dct:type <http://data.lblod.gift/concepts/form-data-file-type> .
-  ?physicalFile nie:dataSource ?logicalFile .
-} LIMIT 1`;
+    SELECT ?submission ?logicalFile ?physicalFile ?submittedDocument
+    WHERE {
+      BIND (${sparqlEscapeUri(uri)} as ?submission)
+      ?submission dct:subject ?submittedDocument .
+      ?submittedDocument dct:source ?logicalFile .
+      ?logicalFile
+        dct:type <http://data.lblod.gift/concepts/form-data-file-type> .
+      ?physicalFile nie:dataSource ?logicalFile .
+    } LIMIT 1`;
 }
 
 export function createSubmissionFromSubmittedResourceQuery(uuid) {
   return `
-PREFIX dct: <http://purl.org/dc/terms/>
-PREFIX mu:  <http://mu.semte.ch/vocabularies/core/>
-PREFIX nie: <http://www.semanticdesktop.org/ontologies/2007/01/19/nie#>
+    PREFIX dct: <http://purl.org/dc/terms/>
+    PREFIX mu:  <http://mu.semte.ch/vocabularies/core/>
+    PREFIX nie: <http://www.semanticdesktop.org/ontologies/2007/01/19/nie#>
 
-SELECT ?submission ?logicalFile ?physicalFile ?submittedDocument
-WHERE {
-  ?submittedDocument mu:uuid ${sparqlEscapeString(uuid)} .
-  ?submission dct:subject ?submittedDocument .
-  ?submittedDocument dct:source ?logicalFile .
-  ?logicalFile dct:type <http://data.lblod.gift/concepts/form-data-file-type> .
-  ?physicalFile nie:dataSource ?logicalFile .
-} LIMIT 1`;
+    SELECT ?submission ?logicalFile ?physicalFile ?submittedDocument
+    WHERE {
+      ?submittedDocument mu:uuid ${sparqlEscapeString(uuid)} .
+      ?submission dct:subject ?submittedDocument .
+      ?submittedDocument dct:source ?logicalFile .
+      ?logicalFile
+        dct:type <http://data.lblod.gift/concepts/form-data-file-type> .
+      ?physicalFile nie:dataSource ?logicalFile .
+    } LIMIT 1`;
 }
 
 // TODO ask about the dct:type of file.
 export function createSubmissionFromAutoSubmissionTaskQuery(uri) {
   return `
-PREFIX dct:  <http://purl.org/dc/terms/>
-PREFIX prov: <http://www.w3.org/ns/prov#>
-PREFIX adms: <http://www.w3.org/ns/adms#>
-PREFIX task: <http://redpencil.data.gift/vocabularies/tasks/>
-PREFIX nie:  <http://www.semanticdesktop.org/ontologies/2007/01/19/nie#>
+    PREFIX dct:  <http://purl.org/dc/terms/>
+    PREFIX prov: <http://www.w3.org/ns/prov#>
+    PREFIX adms: <http://www.w3.org/ns/adms#>
+    PREFIX task: <http://redpencil.data.gift/vocabularies/tasks/>
+    PREFIX nie:  <http://www.semanticdesktop.org/ontologies/2007/01/19/nie#>
 
-SELECT ?submission ?logicalFile ?physicalFile ?submittedDocument
-WHERE {
-  ${sparqlEscapeUri(uri)}
-    a task:Task ;
-    dct:isPartOf ?job .
-  ?job prov:generated ?submission .
-  ?submission dct:subject ?submittedDocument .
-  ?submittedDocument dct:source ?logicalFile .
-  ?logicalFile dct:type <http://data.lblod.gift/concepts/form-data-file-type> .
-  ?physicalFile nie:dataSource ?logicalFile .
-} LIMIT 1`;
+    SELECT ?submission ?logicalFile ?physicalFile ?submittedDocument
+    WHERE {
+      ${sparqlEscapeUri(uri)}
+        a task:Task ;
+        dct:isPartOf ?job .
+      ?job prov:generatedBy ?submission .
+      ?submission dct:subject ?submittedDocument .
+      ?submittedDocument dct:source ?logicalFile .
+      ?logicalFile
+        dct:type <http://data.lblod.gift/concepts/form-data-file-type> .
+      ?physicalFile nie:dataSource ?logicalFile .
+    } LIMIT 1`;
 }
 
-export function insertFormDataQuery({uri, uuid, submission, properties}) {
+export function insertFormDataQuery({ uri, uuid, submission, properties }) {
   let statusSendTriple;
 
-  if(submission.isStatusSend) {
+  if (submission.isStatusSend) {
     statusSendTriple = `
-     ${sparqlEscapeUri(uri)} ext:formSubmissionStatus ${sparqlEscapeUri(STATUS_SEND_CONCEPT)}.
+     ${sparqlEscapeUri(uri)}
+      ext:formSubmissionStatus ${sparqlEscapeUri(STATUS_SEND_CONCEPT)}.
     `;
   }
 
   return `
-PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
-PREFIX mu: <http://mu.semte.ch/vocabularies/core/>
-PREFIX meb: <http://rdf.myexperiment.org/ontologies/base/>
-PREFIX ext: <http://mu.semte.ch/vocabularies/ext/>
+    PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+    PREFIX mu: <http://mu.semte.ch/vocabularies/core/>
+    PREFIX meb: <http://rdf.myexperiment.org/ontologies/base/>
+    PREFIX ext: <http://mu.semte.ch/vocabularies/ext/>
 
-INSERT {
-  GRAPH ?g {
-    ${sparqlEscapeUri(uri)} a ${sparqlEscapeUri(MELDING('FormData').value)} .
-    ${sparqlEscapeUri(uri)} mu:uuid ${sparqlEscapeString(uuid)} .
-    ${properties.map((property) => property.toNT(uri)).join('\n    ')}
-    ${sparqlEscapeUri(uri)} ${sparqlEscapeUri(PROV('hadPrimarySource').value)} ${sparqlEscapeUri(submission.ttl.uri)} .
-    ${sparqlEscapeUri(submission.uri)} ${sparqlEscapeUri(PROV('generated').value)} ${sparqlEscapeUri(uri)} .
-    ${statusSendTriple || ""}
-  }
-} WHERE {
-  GRAPH ?g {
-    ${sparqlEscapeUri(submission.uri)} a meb:Submission .
-  }
-}`;
+    INSERT {
+      GRAPH ?g {
+        ${sparqlEscapeUri(uri)}
+          a ${sparqlEscapeUri(MELDING('FormData').value)} ;
+          mu:uuid ${sparqlEscapeString(uuid)} .
+        ${properties.map((property) => property.toNT(uri)).join('\n    ')}
+        ${sparqlEscapeUri(uri)}
+          ${sparqlEscapeUri(PROV('hadPrimarySource').value)}
+            ${sparqlEscapeUri(submission.ttl.uri)} .
+        ${sparqlEscapeUri(submission.uri)}
+          ${sparqlEscapeUri(PROV('generated').value)} ${sparqlEscapeUri(uri)} .
+        ${statusSendTriple || ''}
+      }
+    } WHERE {
+      GRAPH ?g {
+        ${sparqlEscapeUri(submission.uri)} a meb:Submission .
+      }
+    }`;
 }
 
 export function completeFormDataFromSubmissionQuery(uri) {
   return `
-PREFIX mu: <http://mu.semte.ch/vocabularies/core/>
-PREFIX meb: <http://rdf.myexperiment.org/ontologies/base/>
-PREFIX prov: <http://www.w3.org/ns/prov#>
-PREFIX melding: <http://lblod.data.gift/vocabularies/automatische-melding/>
+    PREFIX mu: <http://mu.semte.ch/vocabularies/core/>
+    PREFIX meb: <http://rdf.myexperiment.org/ontologies/base/>
+    PREFIX prov: <http://www.w3.org/ns/prov#>
+    PREFIX melding: <http://lblod.data.gift/vocabularies/automatische-melding/>
 
-SELECT DISTINCT ?formDataURI ?formDataUUID
-WHERE {
-  ${sparqlEscapeUri(uri)}
-    a meb:Submission ;
-    prov:generated ?formDataURI .
-  ?formDataURI
-    a melding:FormData ;
-    mu:uuid ?formDataUUID.
-} LIMIT 1`;
+    SELECT DISTINCT ?formDataURI ?formDataUUID
+    WHERE {
+      ${sparqlEscapeUri(uri)}
+        a meb:Submission ;
+        prov:generated ?formDataURI .
+      ?formDataURI
+        a melding:FormData ;
+        mu:uuid ?formDataUUID.
+    } LIMIT 1`;
 }
 
 export function deleteFormDataQuery(uri) {
   return `
-PREFIX melding: <http://lblod.data.gift/vocabularies/automatische-melding/>
+    PREFIX melding: <http://lblod.data.gift/vocabularies/automatische-melding/>
 
-DELETE WHERE {
-  GRAPH ?g {
-  ${sparqlEscapeUri(uri)}
-    a melding:FormData ;
-    ?predicate ?object .
-  }
-}`;
+    DELETE WHERE {
+      GRAPH ?g {
+      ${sparqlEscapeUri(uri)}
+        a melding:FormData ;
+        ?predicate ?object .
+      }
+    }`;
 }
 
 export function deleteFormDataFromSubmissionQuery(uri) {
   return `
-PREFIX mu: <http://mu.semte.ch/vocabularies/core/>
-PREFIX meb: <http://rdf.myexperiment.org/ontologies/base/>
-PREFIX prov: <http://www.w3.org/ns/prov#>
-PREFIX melding: <http://lblod.data.gift/vocabularies/automatische-melding/>
+    PREFIX mu: <http://mu.semte.ch/vocabularies/core/>
+    PREFIX meb: <http://rdf.myexperiment.org/ontologies/base/>
+    PREFIX prov: <http://www.w3.org/ns/prov#>
+    PREFIX melding: <http://lblod.data.gift/vocabularies/automatische-melding/>
 
-DELETE {
-  GRAPH ?g {
-    ${sparqlEscapeUri(uri)} prov:generated ?formData .
-    ?formData ?p ?o .
-  }
-}
-WHERE {
-  GRAPH ?g {
-    ${sparqlEscapeUri(uri)}
-      a meb:Submission ;
-      prov:generated ?formData .
-    ?formData ?p ?o .
-  }
-}`;
+    DELETE {
+      GRAPH ?g {
+        ${sparqlEscapeUri(uri)} prov:generated ?formData .
+        ?formData ?p ?o .
+      }
+    }
+    WHERE {
+      GRAPH ?g {
+        ${sparqlEscapeUri(uri)}
+          a meb:Submission ;
+          prov:generated ?formData .
+        ?formData ?p ?o .
+      }
+    }`;
 }
 
 export function retrieveCodeListQuery(uri) {
   return `
-SELECT ?concept
-WHERE {
-  ?concept <http://www.w3.org/2004/02/skos/core#inScheme> ${sparqlEscapeUri(uri)}
-}`;
+    SELECT ?concept
+    WHERE {
+      ?concept
+        <http://www.w3.org/2004/02/skos/core#inScheme> ${sparqlEscapeUri(uri)}
+    }`;
 }
+
 export function askSubmissionSendStatus(submissionUri) {
   return `
     ASK {
